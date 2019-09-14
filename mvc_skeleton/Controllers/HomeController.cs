70e081678e34e85955aabb466686c89b3c2284dd
provaakter <cseerani@gmail.com>
2019-09-14 04:36:03 +0200

    comenting for git test

--- mvc_skeleton/Controllers/HomeController.cs
+++ mvc_skeleton/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace mvc_skeleton.Controllers
         public ActionResult Index()
         {
             return View();
+            //git test purpose
         }
 
         public ActionResult About()
